fa3aec037c15d10532888ac1bcf3279b9abbe684
Corey Richard <ullrtheglorious@gmail.com>
2018-03-24 14:26:09 -0700

    fixed JS transitions for the hex's

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ var lFollowX = 0,
     x = 0,
     y = 0,
     friction = 1 / 30;
+var lastClicked = 'home';
 
 function moveBackground() {
     x += (lFollowX - x) * friction;
@@ -32,9 +33,34 @@ $(window).on('mousemove click', function (e) {
 
 moveBackground();
 // moving the buttons
-var transition = false;
-
+//var transition = false;
+// on click for "home" reset top three button 
+$("#about").unbind("click").click(function () {
+    if(lastClicked === 'home'){
+        myMove();
+    }
+    lastClicked = 'about';
+    console.log("clicked")
+})
+// on click for "contact me" to move top buttons
+$("#contact").unbind("click").click(function () {
+    if (lastClicked === 'home') {
+        myMove();
+    }
+    lastClicked = 'contact';
+    console.log("clicked")
+})
+// on click for "about me" to move top buttons
+$("#home").click(function () {
+    if (lastClicked !== 'home') {
+        reset();
+    }
+    lastClicked = 'home';
+    console.log("clicked")
+})
+// my move function for moving all three buttons
 function myMove() {
+    var transition = false;
     $(".trans").fadeOut("slow");
     myMove2();
     myMove3();
@@ -44,29 +70,34 @@ function myMove() {
     function frame() {
         if (pos == 440 || transition == true) {
             clearInterval(id);
+            transition = true;
         } else {
-            pos+= 4;
+            pos += 4;
             elem.style.right = pos + 'px';
-            setTimeout(time, 2000)
+            //setTimeout(time, 2000)
         }
     }
 }
+// move 2 linked into main move function
 function myMove2() {
+    var transition = false;
     var elem = document.getElementById("animate2");
     var pos = 0;
     var id = setInterval(frame, 1);
     function frame() {
         if (pos == 440 || transition == true) {
             clearInterval(id);
-          
+            transition = true;
         } else {
             pos+= 4;
             elem.style.right = pos + 'px';
-            setTimeout(time, 2000)
+            //setTimeout(time, 2000)
         }
     }
 }
+// move 3 linked into main move function, need two for distance differences 
 function myMove3() {
+    var transition = false;
     var elem = document.getElementById("animate3");
     var x = 0;
     var y = 0;
@@ -74,21 +105,18 @@ function myMove3() {
     function frame() {
         if (x == 700 || y == 136 || transition == true) {
             clearInterval(id);
-            
+            transition = true;
         } else {
             x += 5;
             y ++;
             elem.style.top = y + 'px';
             elem.style.right = x + 'px';
-            setTimeout(time, 2000)
+            //setTimeout(time, 2000)
             
         }
     }
 }
-function time() {
-    transition = true;
-}
-
+// reset funciton to bring both buttons back to starting positions
 function reset() {
     $(".trans").fadeIn("slow");
     var elem = document.getElementById("animate");
@@ -101,9 +129,12 @@ function reset() {
         elem.style = pos;
         elem1.style = pos;
         elem2.style = pos;
-        setTimeout(time2, 2000)
-        }
-}
-function time2() {
-    transition = false;
-}
+        time2();
+    }
+}  
+// function time() {
+//     transition = true;
+// }
+// function time2() {
+//     transition = false;
+// }
