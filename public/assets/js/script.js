a216d608cda6754a6c8812b2b2b85afe66a70748
Corey Richard <ullrtheglorious@gmail.com>
2018-06-05 17:27:13 -0700

    moving pages and links

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,24 +1,9 @@
 $(document).ready(function() {
-    $(".about").hide()
-    $(".aboutLink").hide()
-    $(".techLink").hide()
-    $(".tech").hide()
     $(".icon-scroll").hide()
 
-    setTimeout(function () { 
-        $(".aboutLink").fadeIn("slow"); 
-    }, 2000);
+
     setTimeout(function () {
         $(".icon-scroll").fadeIn("slow");
-    }, 4000);
-    setTimeout(function () {
-        $(".techLink").fadeIn("slow");
-    }, 3000);
+    }, 1000);
 
-    $(".aboutLink").click(function(){
-        window.location.replace('#page3Break');
-    })
-    $(".techLink").click(function () {
-        window.location.replace('#page3Break');;
-    })
 })
